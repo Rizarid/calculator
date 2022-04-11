20ccb119daebbde8dc8fb66db020894f6ab505cd
Konstantin Lozhkin <k.lozhkin@unitedtraders.com>
2022-04-11 21:50:15 +0500

    added component InsertionPlace

---
+++ src/components/insertion-place/InsertionPlace.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './InsertionPlace.css';
+
+export const InsertionPlace: React.FC = () => (
+  <div className="insertion-place">
+    <span className="insertion-place__rhomb" />
+    <span className="insertion-place__line" />
+    <span className="insertion-place__rhomb" />
+  </div>
+);
