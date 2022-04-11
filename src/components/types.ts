b043556a54ccbf44053db6e27065fb93be3df920
Konstantin Lozhkin <k.lozhkin@unitedtraders.com>
2022-04-11 21:38:01 +0500

    added types of components

---
+++ src/components/types.ts
@@ -0,0 +1,9 @@
+export const DISPLAY_NAME = 'display';
+export const NUMBERS_BLOCK_NAME = 'numberBlock';
+export const OPERATIONS_BLOCK_NAME = 'operationsBlock';
+export const RESULT_BUTTON_NAME = 'resultButton';
+export const INSERTION_PLACE_NAME = 'insertionPlace';
+
+export interface ComponentProps {
+  fromPalette?: boolean;
+}
